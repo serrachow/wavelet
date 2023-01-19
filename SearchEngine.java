1fc1b7199a452d7f0092de0c45a86359855605f7
Serena Chuang <tzyharnchuang@gmail.com>
2023-01-18 16:25:08 -0800

    Create SearchEngine.java

---
+++ SearchEngine.java
@@ -0,0 +1,39 @@
+import java.io.IOException;
+import java.net.URI;
+
+class Handler implements URLHandler {
+    // The one bit of state on the server: a number that will be manipulated by
+    // various requests.
+    String startingString = " ";
+
+    public String handleRequest(URI url) {
+        if (url.getPath().equals("/")) {
+            return String.format("Serena's String: %d", startingString);
+        // } else if (url.getPath().equals("/search")) {
+            
+        } else {
+            System.out.println("Path: " + url.getPath());
+            if (url.getPath().contains("/add")) {
+                String[] parameters = url.getQuery().split("=");
+                if (parameters[0].equals("s")) {
+                    startingString = startingString + " " + parameters[1];
+                    return String.format("Added %s!", parameters[1]);
+                }
+            }
+            return "404 Not Found!";
+        }
+    }
+}
+
+class SearchEngine {
+    public static void main(String[] args) throws IOException {
+        if(args.length == 0){
+            System.out.println("Missing port number! Try any number between 1024 to 49151");
+            return;
+        }
+
+        int port = Integer.parseInt(args[0]);
+
+        Server.start(port, new Handler());
+    }
+}
